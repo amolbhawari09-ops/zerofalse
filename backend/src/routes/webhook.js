da717196eb8cf7885811fb01da91cc0560921de9
amolbhawari09-ops <amolbhawari09@gmail.com>
2026-02-07 15:39:28 +0530

    Update webhook.js

--- backend/src/routes/webhook.js
+++ backend/src/routes/webhook.js
@@ -1,16 +1,8 @@
 const express = require('express');
 const router = express.Router();
+
 const WebhookController = require('../controllers/webhookController');
 
-/*
-IMPORTANT:
-GitHub requires RAW body for signature verification
-*/
-
-router.post(
-  '/github',
-  express.raw({ type: 'application/json' }),
-  WebhookController.handleGitHubWebhook.bind(WebhookController)
-);
+router.post('/github', WebhookController.handleGitHubWebhook.bind(WebhookController));
 
 module.exports = router;
